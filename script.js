d6d66324f8a4e10baa3a6887144164613c6d9b71
Natasha Beddow <beddow90@gmail.com>
2022-10-03 16:42:49 +0100

    added variables for ball movement

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+// initial starting point for ball
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+// add to initial starting point to get ball to move
+let dx = 2;
+let dy = -2;
+
+function draw() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
