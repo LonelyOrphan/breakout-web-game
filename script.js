fcb7925caa4c42d7f26fb1ada4121af6f98f4774
Natasha Beddow <beddow90@gmail.com>
2022-10-05 15:04:50 +0100

    added ball colour change

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const ballRadius = 10;
+let ballColor = "green";
 
 // initial starting point for ball
 let x = canvas.width / 2;
@@ -9,12 +11,10 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-const ballRadius = 10;
-
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "green";
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -27,10 +27,12 @@ function draw() {
   // Bounce off side walls
   if (x + dx > canvas.width - ballRadius || x + dx < 0) {
     dx = -dx;
+    ballColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
   // Bounce off top and bottom walls
   if (y + dy > canvas.height - ballRadius || y + dy < 0) {
     dy = -dy;
+    ballColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 }
 
